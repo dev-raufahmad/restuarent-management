5b97bec415e755032bec2cbcc1dd465e961c206d
dev-raufniazi <devraufahmadniazi@gmail.com>
2026-09-04 11:33:14 +0500

    user dashboard completed

--- backend/route/restaurant.route.js
+++ backend/route/restaurant.route.js
@@ -1,9 +1,14 @@
 const express = require('express');
-const { getRestauants , comments } = require('../controller/restaurant.controller.js');
+const {bookingRequest , getRestauants ,reserveTable, comments , availableTimes, upcomingRequest, diningHistory, recommendedRestaurants } = require('../controller/restaurant.controller.js');
 
 const router = express.Router();
 
 router.get('/' , getRestauants );
 router.get('/comments/:id' , comments );
-
+router.get('/available-times/:id/:date' , availableTimes );
+router.post("/reserve/:id" , reserveTable);
+router.get('/bookings/:id' , bookingRequest);
+router.get('/upcoming-bookings/:id' , upcomingRequest)
+router.get('/dining-history/:id' , diningHistory)
+router.get('/recommended' , recommendedRestaurants)
 module.exports = router;
